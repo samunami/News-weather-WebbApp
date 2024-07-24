9fba751d1884132b836381571cf1ac21d4155ae3
samunami <salamon51020@gmail.com>
2024-07-24 23:14:36 +0300

    new config file example

---
+++ config.example.py
@@ -0,0 +1,5 @@
+
+
+WEATHER_URL = 'http://api.weatherapi.com/v1/current.json'
+WEATHER_API_KEY = 'your_api_key_here'  # Замените своим действующим API-ключом
+WEATHER_DEFAULT_CITY = 'Rostov-on-Don,Russia'
